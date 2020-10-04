e087e3ad362577151fd5c990ffd201bbd9793037
SMYALTAMASH <smy.altamash@gmail.com>
2020-10-04 13:47:00 +0530

    requestHeadersQueryParamsAndBody.go

--- requestHeadersQueryParamsAndBody.go
+++ requestHeadersQueryParamsAndBody.go
@@ -4,6 +4,7 @@ import (
     "fmt"
     "log"
     "net/http"
+    "strings"
     "os"
     "io/ioutil"
 )
@@ -11,9 +12,13 @@ import (
 func indexHandler(w http.ResponseWriter, r *http.Request) {
   var bodyBytes []byte
   var final string
+  var qparams, curlheaders string
+
   final += "###################################################################\n"
+
   final += "HTTP Method:  "+r.Method+"\n"
   final += "REQUEST URL:  "+r.URL.Path+"\n"
+
   if r.Body != nil {
       bodyBytes, _ = ioutil.ReadAll(r.Body)
   }
@@ -21,23 +26,46 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
   final += "BODY:         "+string(bodyBytes)+"\n"
   for key, val := range r.URL.Query() {
     final += "Query Param:  "+key+" = "+val[0]+"\n"
+    if qparams != "" {
+      qparams += "&"+key+"="+val[0]
+    } else {
+      qparams = "?"+key+"="+val[0]
+    }
   }
+
+  qparams += "' \\\n"
   for name, values := range r.Header {
       for _, value := range values {
         final += "HEADER:       "+name+" = "+value+"\n"
+        if curlheaders != "" {
+          curlheaders += "\n-H '"+name+": "+value+"' \\"
+        } else {
+          curlheaders += "-H '"+name+": "+value+"' \\"
+        }
       }
   }
+
+  if string(bodyBytes) == "" {
+    final += "CURL COMMAND: \ncurl -X"+r.Method+" '"+r.Host+""+r.URL.Path+""+qparams+""+strings.TrimSuffix(curlheaders, "\\")+"\n\n"
+  } else {
+    final += "CURL COMMAND: \ncurl -X"+r.Method+" '"+r.Host+""+r.URL.Path+""+qparams+""+curlheaders+"--data-urlencode: '"+string(bodyBytes)+"'\n\n"
+  }
+
   final += "###################################################################\n"
+
   f, err := os.OpenFile("/tmp/requestHeadersQueryParamsAndBody.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
   if err != nil {
       log.Fatal(err)
   }
+
   if _, err := f.Write([]byte(final)); err != nil {
       log.Fatal(err)
   }
+
   if err := f.Close(); err != nil {
       log.Fatal(err)
   }
+
   fmt.Fprintf(w, string(bodyBytes))
 }
 
